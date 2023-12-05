59c3bc15d7ec83493cb507796dc386c89b0a4578
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-12-05 13:57:56 -0500

    Update write a program to find greatest of 3 numbers.java

--- write a program to find greatest of 3 numbers.java
+++ write a program to find greatest of 3 numbers.java
@@ -3,25 +3,25 @@ public class NewMain {
     
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);   // initialization for Scanner class
         
-         System.out.println("Enter the three numbers");
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int num3 = sc.nextInt();
+         System.out.println("Enter the three numbers"); // asking user to enter number to see which mumber is the greatest amoung all the numbers
+        int num1 = sc.nextInt();  // declasring and initializing the int value
+        int num2 = sc.nextInt(); // declasring and initializing the int value
+        int num3 = sc.nextInt(); // declasring and initializing the int value
         
         
-        if(num1>num2 && num1> num3){
+        if(num1>num2 && num1> num3){  // if statment is used to see if the num1 has the grtest value 
             
-            System.out.println("num1 is the greatest number");
+            System.out.println("num1 is the greatest number");  // if num1 is gretest print num1 is the greatest number
         }
         
-        else if(num2 > num1 && num2 > num3){
-         System.out.println("num2 is the greatest number");
+        else if(num2 > num1 && num2 > num3){  // if the 'if' statement is false than it comes to else to see num2 is the gretest value
+         System.out.println("num2 is the greatest number"); // if num1 is gretest print num2 is the greatest number
     }
         
-        else{
-             System.out.println("num3 is the greatest number");
+        else{  // if both conditionn is false, than it comes to else and print num3 is the greatest
+             System.out.println("num3 is the greatest number");// if num1 is gretest print num3 is the greatest number
         }
         
     }
