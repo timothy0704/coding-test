7fe9e9b542d23d2ce1cb40c3909fa8f36d93ee9b
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-12-05 13:47:13 -0500

    Update Write a program to check whehter a number is even or odd.java

--- Write a program to check whehter a number is even or odd.java
+++ Write a program to check whehter a number is even or odd.java
@@ -1,22 +1,22 @@
-import java.util.Scanner;
+import java.util.Scanner; // declaring Scanner class which allows user to input value
 public class NewMain {
     
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);  // initialization for Scanner class
         
         
-         System.out.println("Enter the number");
+         System.out.println("Enter the number");  // asking user to enter a number to determine the if the numebr is even or odd
          
-        double num = sc.nextDouble();
+        double num = sc.nextDouble(); // declaring and initializing the double value
         
-        if(num%2==0){
-            System.out.println("The number is even");
+        if(num%2==0){  // if the number that user enter has no remainder
+            System.out.println("The number is even");  // print even number
     
         }
         
-        else{
-             System.out.println("The number is odd");
+        else{  // if the number that user enter has remainder
+             System.out.println("The number is odd");  // print odd number 
         }
         
     }
